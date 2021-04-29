fa4953bfa2e438c15e494e9c48516e2c34baa59c
Mattia Roccoberton <mattiaroccoberton@nebulab.it>
2021-04-29 23:56:12 +0200

    Initialize JS components
    - Apply selectize.js to the select inputs
    - Enable jquery + jquery_ujs for Destroy buttons

--- app/assets/javascripts/administrate-bootstrap-theme/theme.js
+++ app/assets/javascripts/administrate-bootstrap-theme/theme.js
@@ -1,2 +1,11 @@
-//= require bootstrap/dist/js/bootstrap.bundle
+//= require jquery
+//= require jquery_ujs
 
+//= require bootstrap/dist/js/bootstrap.bundle
+//= require selectize
+
+document.addEventListener('DOMContentLoaded', function(_event) {
+  $('.field-unit--belongs-to select').selectize({});
+  $('.field-unit--has-many select').selectize({});
+  $('.field-unit--polymorphic select').selectize({});
+});
