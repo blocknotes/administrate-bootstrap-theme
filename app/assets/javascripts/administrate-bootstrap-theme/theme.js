f26f9e6fa2e08dc18351f03a8aed22780ce17e6e
Mattia Roccoberton <mattiaroccoberton@nebulab.it>
2021-04-30 00:01:42 +0200

    Add flatpickr for date selectors

--- app/assets/javascripts/administrate-bootstrap-theme/theme.js
+++ app/assets/javascripts/administrate-bootstrap-theme/theme.js
@@ -2,9 +2,16 @@
 //= require jquery_ujs
 
 //= require bootstrap/dist/js/bootstrap.bundle
+//= require flatpickr
 //= require selectize
 
 document.addEventListener('DOMContentLoaded', function(_event) {
+  if(typeof window.flatpickr === 'function') {
+    var options = window.flatpickr_filters_options;
+    if(typeof options !== 'object') options = { enableTime: true };
+    $('[data-type="date"]').flatpickr(options);
+  }
+
   $('.field-unit--belongs-to select').selectize({});
   $('.field-unit--has-many select').selectize({});
   $('.field-unit--polymorphic select').selectize({});
