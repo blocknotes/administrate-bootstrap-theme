9b73d0659223d6c56136e764428a81034357970f
Mattia Roccoberton <mattia.roccoberton@telepass.com>
2024-07-03 08:54:03 +0200

    test: precompile assets in dummy app

--- spec/dummy/config/environments/test.rb
+++ spec/dummy/config/environments/test.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
   # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
   config.public_file_server.headers = {
-    'Cache-Control' => "public, max-age=#{1.hour.to_i}"
+    'Cache-Control' => "public, max-age=3600"
   }
 
   # Show full error reports and disable caching.
@@ -32,7 +32,7 @@ Rails.application.configure do
   config.action_controller.allow_forgery_protection = false
 
   # Store uploaded files on the local file system in a temporary directory.
-  config.active_storage.service = :test
+  config.active_storage.service = :test if config.respond_to? :active_storage
 
   # config.action_mailer.perform_caching = false
 
